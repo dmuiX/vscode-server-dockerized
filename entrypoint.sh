b9569997973ee4d4bbaaf31c8eb580d19e8acb85
Daniel Ullrich <19862760+dmuiX@users.noreply.github.com>
2025-03-12 00:37:13 +0100

    Update entrypoint.sh
    missing [] for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set password directly from file if specified
-[ -n "$USER_PASSWORD_FILE" && -f "$USER_PASSWORD_FILE" ] || { echo "Error: Password file $USER_PASSWORD_FILE not found"; exit 1; }
+[[ -n "$USER_PASSWORD_FILE" && -f "$USER_PASSWORD_FILE" ]] || { echo "Error: Password file $USER_PASSWORD_FILE not found"; exit 1; }
 usermod --password $(cat "$USER_PASSWORD_FILE" | openssl passwd -1 -stdin) vscode
 
 /opt/code --user-data-dir /home/vscode/.vscode/user-data --extensions-dir /home/vscode/.vscode/extensions serve-web --without-connection-token --accept-server-license-terms --host 0.0.0.0 --port 8000 --cli-data-dir /home/vscode/.vscode/cli-data --server-data-dir /home/vscode/.vscode/server-data
